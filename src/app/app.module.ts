2fc98d60f1ba1e8b3c6b9431d5d64124755ed47f
Kelber Stuchi <kelberstuchi@gmail.com>
2019-07-06 17:58:28 -0300

    added comments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Components
+import { PostsComponent } from './posts/posts.component';
 import { ReadComponent } from './read/read.component';
 import { CreateComponent } from './create/create.component';
 
+// NGXS 
 import { NgxsModule } from '@ngxs/store';
-import { TutorialState,  } from './state/tutorial.state';
 import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
 import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
-
 import { NgxsRouterPluginModule } from '@ngxs/router-plugin';
-import { PostsComponent } from './posts/posts.component';
+
+// States
+import { TutorialState } from './state/tutorial.state';
 
 @NgModule({
   declarations: [
